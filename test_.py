c602d04a65264b20ed6f353773db28f22eb5efcf
Taya Medvedeva <71186576+MTasia@users.noreply.github.com>
2021-02-03 13:44:51 +0300

    Update test_.py

--- test_.py
+++ test_.py
@@ -1,12 +1,11 @@
 import os
 import sys
-import unittest
+import pytest
 
 import src.dir_reader as d_r
 import src.file_reader as f_r
 
 
-class TestF(unittest.TestCase):
-    def test_DirReader(self):
-        for file in d_r.DirReader("test_dir"):
-            self.assertEqual(file, "test_dir"+"\\test.txt")
+def test_DirReader():
+    for file in d_r.DirReader("test_dir"):
+        assert(file=="test_dir"+"\\test.txt")
